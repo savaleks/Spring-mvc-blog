2a0233066494b4a3f7309e786576bf409bda764d
Alexander <savaleks@gmail.com>
2019-02-06 19:07:58 +0200

    Project Setup

---
+++ mvc-blog/src/main/java/com/savaleks/mvcblog/MvcBlogApplication.java
@@ -0,0 +1,14 @@
+package com.savaleks.mvcblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcBlogApplication.class, args);
+	}
+
+}
+
